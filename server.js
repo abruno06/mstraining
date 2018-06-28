f5d1adf320e3cbe0e2422e8debbbad1191180025
Aurelien Bruno <aurelbruno06@gmail.com>
2018-06-28 13:53:01 +0000

    Add Eureka to test routeing

--- server.js
+++ server.js
@@ -2,6 +2,37 @@ const express = require('express')
 const os = require('os')
 const app = express()
 
-app.get('/', (req, res) => res.send(`hello hostname: ${os.hostname}`))
+const Eureka = require('eureka-js-client').Eureka;
+// example configuration
+const client = new Eureka({
+  "instance": {
+    "app": "helloservice",
+    "hostName": "rpi-ab",
+    "ipAddr": "172.20.61.23",
+     "port": {
+      '$': 3000,
+      '@enabled': true
+    },
+    "vipAddress": "172.20.61.23",
+    "dataCenterInfo": {
+  '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      "name": "MyOwn"
+    }
+  },
+  "eureka": {
+    "host": "172.20.61.29",
+    "port": 8080
+  }
+});
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.get('/', (req, res) => res.send(`{"servername": "${os.hostname}"`))
+
+app.listen(3002, () => console.log('listening on port 3002'))
+
+function connectToEureka() {              
+    client.logger.level('debug');  
+    client.start(function(error) {
+    console.log('########################################################');
+    console.log(JSON.stringify(error) || 'Eureka registration complete');   }); }
+
+ connectToEureka();
