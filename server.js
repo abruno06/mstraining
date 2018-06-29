0f175a6ce5611dadf0d8bc408b76e61b03c4749d
Aurelien Bruno <aurelbruno06@gmail.com>
2018-06-29 12:07:51 +0000

    add eureka configuration

--- server.js
+++ server.js
@@ -6,30 +6,31 @@ const Eureka = require('eureka-js-client').Eureka;
 
 // example configuration
 var clientInformation = {
-  "instance": {
-    "app": "helloservice",
-    "hostName": os.hostname(),
-    "ipAddr": "172.20.61.23",
-     "port": {
-      '$': 3000,
-      '@enabled': true
+  'instance': {
+    'app': process.env.APP,
+    'hostName': '172.20.61.23',
+    'ipAddr': '172.20.61.23',
+     'port': {
+      '$': 3002,
+      '@enabled': 'true'
     },
-    "vipAddress": "172.20.61.23",
-    "dataCenterInfo": {
-  '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-      "name": "MyOwn"
+    'vipAddress': '172.20.61.23',
+    'dataCenterInfo': {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      'name': 'MyOwn'
     }
   },
-  "eureka": {
-    "host": process.env.EUREKASRV,
-    "port": process.env.EUREKAPORT
+  eureka: {
+    'host': process.env.EUREKASRV,
+    'port': process.env.EUREKAPORT,
+'servicePath': '/eureka/apps/'
   }
 };
 
 console.log(clientInformation);
 const client = new Eureka(clientInformation);
 
-app.get('/', (req, res) => res.send(`{"servername": "${os.hostname}"}`))
+app.get('/ping', (req, res) => res.send(`{"servername": "${os.hostname}"}`))
 
 app.listen(3002, () => console.log('listening on port 3002'))
 
@@ -39,4 +40,21 @@ function connectToEureka() {
     console.log('########################################################');
     console.log(JSON.stringify(error) || 'Eureka registration complete');   }); }
 
- connectToEureka();
+
+
+connectToEureka();
+
+
+//process.on('SIGINT', function() {
+//    console.log("Caught interrupt signal");
+//
+//     if (i_should_exit)
+//{    client.stop(function(error) {
+//    console.log('########################################################');
+//    console.log(JSON.stringify(error) || 'Eureka registration stopped');  
+// process.exit();
+// });
+//}
+//});
+
+
